c73e7ef54c8007706ca0bb52224230021c2e751b
Jordan <jspeed8th@gmail.com>
2018-12-04 14:58:20 -0500

    Created Edit, and Delete User functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(current_user.id)
     @parties = @user.events
   end
 
-  def show
-  	@user = User.find(params[:id])
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    user = User.find(current_user.id)
+    if user.update(user_params)
+      redirect_to '/users/current_user.id'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      redirect_to '/'
+    end
   end
 
   private
