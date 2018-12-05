433c0710a40bb68a56aed0f64b0f7ef8ba21e64c
acappello459 <acappello459@outlook.com>
2018-12-05 10:58:34 -0500

    added in som efuncitonaltiy on the invites page no styling

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,11 +1,19 @@
 class InvitesController < ApplicationController
 
+	def index
+		@invites = Invite.all
+		event = Event.all
+		user = User.all
+		location = Location.all
+	end
+
+
 	def assign
 		eventId = params[:evid].to_i
 		userId = params[:userid].to_i
 		locationId = params[:loc].to_i
 		host = false
-		#if the parameter in the URI is set to true we know they are a host and have been sent here from the location controller 
+		#if the parameter in the URI is set to true we know they are a host and have been sent here from the location controller
 		if params[:host] == "true"
 			host = true
 
@@ -17,7 +25,7 @@ class InvitesController < ApplicationController
 			end
 
 		else #when guest invite is being populated
-			newInvite = Invite.new(event_id: eventId, user_id: userId, location_id: locationId, status: "pending", host: host)			
+			newInvite = Invite.new(event_id: eventId, user_id: userId, location_id: locationId, status: "pending", host: host)
 			if newInvite.save
 				redirect_to "/invites/#{eventId}"
 			else
@@ -32,7 +40,7 @@ class InvitesController < ApplicationController
 		#if button accept is pushed the user has accepted going to the event. Javascript will be used to insure that adress feild has been properly filled out.
 			#run inviteAccepted method to further process information.
 		#if button for decline is pushed, the user has declined going to the event
-			#run  method 
+			#run  method
 	end
 
 end
