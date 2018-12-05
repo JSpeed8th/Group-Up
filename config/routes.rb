bbb750e5555dac0f9881c337b8c6e7b1ae43a655
SethKalkstein <sethkal7@yahoo.com>
2018-12-04 21:57:47 -0500

    didn't create accept behavior, did create conrollers for populating location, event and invite table upon creation of a new event

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :events
+  resources :invites
 
   root "welcome#index"
   post "/assign" => "events#assign"
+  get "/invites/assign/:evid/:userid/:host/:loc" => "invites#assign", :as => :assign_invite
+  get "/locations/assign/:isinvite/:lat/:long/:name" => "locations#assign", :as => :assign_location
+
 
 end
